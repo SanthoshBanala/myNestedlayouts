2ee4d1c20bd34c59d5a0619476ea8badd446ca30
santhosh banala <santhosh.banala1992@gmail.com>
2024-01-01 23:46:59 +0530

    Adding DataBinding feature

--- app/src/main/java/com/example/mynestedlayouts/MainActivity.kt
+++ app/src/main/java/com/example/mynestedlayouts/MainActivity.kt
@@ -10,22 +10,27 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.content.getSystemService
+import androidx.databinding.DataBindingUtil
+import com.example.mynestedlayouts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.btnToConfirm).setOnClickListener{
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //findViewById<Button>(R.id.btnToConfirm).setOnClickListener{
+        //    addNickname(it)
+        //}
+        binding.btnToConfirm.setOnClickListener {
             addNickname(it)
         }
     }
     private fun addNickname (view:View){
-        val editText = findViewById<EditText>(R.id.enterUrname)
-        val nickname = findViewById<TextView>(R.id.textToConfirm)
-        nickname.text=editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickname.visibility = View.VISIBLE
+        binding.textToConfirm.text=binding.enterUrname.text
+        binding.enterUrname.visibility=View.GONE
+        binding.btnToConfirm.visibility=View.GONE
+        binding.textToConfirm.visibility=View.VISIBLE
 
         val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imn.hideSoftInputFromWindow(view.windowToken,0)
